113eb1c46eb7f4279fdd903fbd5b047cec4173e4
Nicholas Ng <onenylxus@gmail.com>
2024-06-17 00:23:00 +0800

    Create readline program

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate rustyline;
+
+use rustyline::{ DefaultEditor, Result };
+use rustyline::error::ReadlineError;
+
+fn main() -> Result<()> {
+  let mut rl = DefaultEditor::new()?;
+
+  if rl.load_history("history.txt").is_err() {
+    eprintln!("ore< history not found");
+  }
+
+  loop {
+    let readline = rl.readline("ore> ");
+    match readline {
+      Ok(line) => {
+        let _ = rl.add_history_entry(&line);
+        rl.save_history("history.txt").unwrap();
+        if line.len() > 0 {
+          println!("ore< {}", line);
+        }
+      },
+      Err(ReadlineError::Interrupted) => {
+        println!("ore< Interrupted");
+        continue
+      },
+      Err(ReadlineError::Eof) => {
+        println!("ore< End Of File");
+        break
+      },
+      Err(err) => {
+        println!("ore< Error: {:?}", err);
+        break;
+      },
+    }
+  }
+
+  Ok(())
+}
